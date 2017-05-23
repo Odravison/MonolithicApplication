7c5f0285c77cb07d5e2cd29121f0a72f0ae9529a
Odravison Amaral <odravison.amaral@dce.ufpb.br>
2017-05-23 11:16:06 -0300

    [LOGGER] Adds logger

--- src/main/java/br/ufpb/odravison/monolithicapplication/web/rest/PingDataService.java
+++ src/main/java/br/ufpb/odravison/monolithicapplication/web/rest/PingDataService.java
@@ -1,5 +1,7 @@
 package br.ufpb.odravison.monolithicapplication.web.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,9 +13,13 @@ public class PingDataService {
     
     @Autowired
 	private PongDataService pongDataService;
+    
+    private Logger logger = LoggerFactory.getLogger(PingDataService.class);
 
     @GetMapping("/ping-data")
 	public ResponseEntity<HttpStatus> pingData(){
+        
+        logger.info("Getting /ping-data");
 		
 		return ResponseEntity.ok(HttpStatus.OK); 
 		
@@ -21,7 +27,9 @@ public class PingDataService {
 	
 	@GetMapping("/ping-data/ping-data-from-pong")
 	public ResponseEntity<HttpStatus> pingDataFromPong(){
-		
+	    
+	    logger.info("Getting /ping-data-from-pong");
+	    
 	    return pongDataService.pongData();
 	}
 
--- src/main/java/br/ufpb/odravison/monolithicapplication/web/rest/PongDataService.java
+++ src/main/java/br/ufpb/odravison/monolithicapplication/web/rest/PongDataService.java
@@ -1,5 +1,7 @@
 package br.ufpb.odravison.monolithicapplication.web.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +14,12 @@ public class PongDataService {
     @Autowired
     private PingDataService pingDataService;
     
+    private Logger logger = LoggerFactory.getLogger(PongDataService.class);
+    
     @GetMapping("/pong-data")
     public ResponseEntity<HttpStatus> pongData(){
+        
+        logger.info("Getting /pong-data");
         
         return ResponseEntity.ok(HttpStatus.OK); 
         
@@ -21,6 +27,8 @@ public class PongDataService {
     
     @GetMapping("/pong-data/pong-data-from-ping")
     public ResponseEntity<HttpStatus> pongDataFromPing(){
+        
+        logger.info("Getting /pong-data-from-ping");
         
         return pingDataService.pingData();
     }
